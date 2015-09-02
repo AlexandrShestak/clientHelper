742d79bc1e39cfaf3423a32051a0dc301c5b106a
AlexandrShestak <shestakalexandr@mail.ru>
2015-09-02 09:54:38 +0300

    add TopicController

---
+++ src/main/java/com/shestakam/topic/controller/TopicController.java
@@ -0,0 +1,29 @@
+package com.shestakam.topic.controller;
+
+import com.shestakam.topic.dao.TopicDao;
+import com.shestakam.topic.entity.Topic;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by shestakam on 1.9.15.
+ */
+@RestController
+public class TopicController {
+
+    private  final static Logger logger = LogManager.getLogger(TopicController.class);
+    private TopicDao topicDao;
+
+    public void setTopicDao(TopicDao topicDao) {
+        this.topicDao = topicDao;
+    }
+
+    @RequestMapping("/topics")
+    public List<Topic> getListTopics() {
+        return topicDao.getAll();
+    }
+}
